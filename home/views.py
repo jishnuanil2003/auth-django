41524ab358d814addee77610b03a25f8138ec456
Jishnuanil <aniljishnu07@gmail.com>
2025-09-04 17:02:00 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+from django.db import IntegrityError
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def loginn(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate (request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            return render(request,'login.html',{'error':'Invalid username or password'})
+    return render(request,'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'signup.html', {'error': 'Username already taken'})
+        elif User.objects.filter(email=email).exists():
+            return render(request, 'signup.html', {'error': 'Email already registered'})
+        else:
+            try:
+                User.objects.create_user(username=username, email=email, password=password)
+                return redirect('login')  
+            except IntegrityError:
+                return render(request, 'signup.html', {'error': 'Something went wrong. Please try again.'})
+
+    return render(request, 'signup.html')
